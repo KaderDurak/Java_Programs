e081860090126d49e402af00e68cb0900151d608
Cosmos <59961853+KaderDurak@users.noreply.github.com>
2021-03-02 15:15:45 +0300

    Add files via upload

---
+++ KDVHesaplama/src/KdvHesaplama.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class KdvHesaplama {
+
+    public static void main(String[] args) {
+        double tutar, kdvliFiyat, kdv = 0.18;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ürünün tutarını giriniz: ");
+        tutar = input.nextDouble();
+        kdvliFiyat = tutar + (tutar*kdv);
+        System.out.println("Ürünün Kdv'li Fiyatı: "+ kdvliFiyat);
+        
+    }
+}
