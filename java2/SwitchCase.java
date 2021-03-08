ea9a5b986148dd891654188267636dd611f8b570
Cosmos <59961853+KaderDurak@users.noreply.github.com>
2021-03-08 23:59:48 +0300

    Add files via upload

---
+++ java2/SwitchCase.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Sayı Giriniz: ");
+        int sayi = scan.nextInt();
+        switch (sayi) {
+        case 1:
+            System.out.println("Sayı 1'e eşittir.");
+            break;
+        case 2:
+            System.out.println("Sayı 2'e eşittir.");
+            break;
+        case 3:
+            System.out.println("Sayı 3'e eşittir.");
+            break;
+        default:
+            System.out.println("Geçersiz sayı girdiniz.");
+    }
+}
+}
