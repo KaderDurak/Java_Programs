78d8a075d7c9127a0aed845d97a890e44c823485
Cosmos <59961853+KaderDurak@users.noreply.github.com>
2021-03-10 17:04:01 +0300

    Add files via upload

---
+++ java2/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+    
+    static int fibo( int n){
+        if(n ==1 || n == 2)
+            return 1;
+        return fibo(n-1) + fibo(n-2);
+    }
+    public static void main(String[] args) {
+        // 1 1 2 3 5 8 13 21 34
+        // f(6) = f(5) + f(4)
+        // f(5) = f(4) + f(3)
+        // f(4) = f(3) + f(2)
+        // f(3) = f(2) + f(1)
+        System.out.println(fibo(9));
+        System.out.println(fibo(8));
+        System.out.println(fibo(7));
+    }
+}
