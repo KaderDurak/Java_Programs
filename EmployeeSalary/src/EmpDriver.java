a48ad8c6c4f24249f87d78d65f530cbc0c856a03
Cosmos <59961853+KaderDurak@users.noreply.github.com>
2021-03-11 17:28:08 +0300

    Add files via upload

---
+++ EmployeeSalary/src/EmpDriver.java
@@ -0,0 +1,7 @@
+public class EmpDriver {
+    public static void main(String[] args) {
+        
+        Employee emp1 = new Employee(1985,45,2000.0,"Kader");
+        emp1.toString(emp1);
+    }
+}
