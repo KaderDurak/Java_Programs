df943e7eb11989acd51665aa5a0b2532505a65a4
Cosmos <59961853+KaderDurak@users.noreply.github.com>
2021-03-30 16:15:47 +0300

    Add files via upload

---
+++ java2/MathSinifi.java
@@ -0,0 +1,23 @@
+public class MathSinifi {
+    public static void main(String[] args) {
+        // Math sınıfı metotları
+        // Math.ceil(x)  => bir büyük tam sayıya yuvarlama yapar.
+        // Math.cos(x)   => cos değerini alır.
+        // Math.sin(x)   => sin değerini alır.
+        // Math.floor(x) => küçük tamsayıya yuvarlar.
+        // Math.max(x,y) => büyük sayıyı verir.
+        // Math.min(x,y) => küçük sayıyı verir.
+        // Math.pow(3,5) => 3^5 üs alır.
+        // Math.random() => 1 ve 0 arasında rastgele değerler alır.
+        // Math.round(x) => yakın olan tam sayıyı verir.
+        // Math.abs(x)   => mutlak değer
+
+
+        System.out.println(Math.ceil(1.6));
+        System.out.println(Math.cos(30));
+        System.out.println(Math.floor(1.9));
+        System.out.println(Math.pow(5,9));
+
+
+    }
+}
